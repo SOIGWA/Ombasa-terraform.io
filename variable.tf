93f0880106e35df5eeee3115946040bb26e5bda7
SOIGWA <andyombasa@gmail.com>
2026-04-13 18:39:04 +0300

    Clean release v0.0.2: Added environment tags

---
+++ variable.tf
@@ -0,0 +1,52 @@
+variable "variables_sub_cidr" {
+  description = "CIDR Block for the Variables Subnet"
+  type        = string
+  default     = "10.0.202.0/24"
+}
+
+variable "variables_sub_az" {
+  description = "Availability Zone used for Variables Subnet"
+  type        = string
+  default     = "us-east-2b"
+}
+
+variable "variables_sub_auto_ip" {
+  description = "Set Automatic IP Assignment for Variables Subnet"
+  type        = bool
+  default     = true
+}
+
+variable "cluster_name" {
+  description = "The name to use for all cluster resources"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for the cluster"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "min_size" {
+  description = "Minimum number of EC2 instances in the ASG"
+  type        = number
+  default = 1
+}
+
+variable "max_size" {
+  description = "Maximum number of EC2 instances in the ASG"
+  type        = number
+  default = 2
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+variable "environment" {
+  description = "The environment for this resource (e.g. dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
