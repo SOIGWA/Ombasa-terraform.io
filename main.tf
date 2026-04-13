7a27a3bafbeef38defc9423353bed00c62b09e78
SOIGWA <andyombasa@gmail.com>
2026-04-13 18:21:29 +0300

    Clean module release - code only

---
+++ main.tf
@@ -0,0 +1,343 @@
+provider "aws" {
+  region                   = "us-west-2"
+  shared_credentials_files = ["C:\\Users\\Admin\\.aws\\credentials"]
+  profile                  = "default"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-west-2"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "demo_vpc"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  default = {
+    "private_subnet_1" = 1
+    "private_subnet_2" = 2
+    "private_subnet_3" = 3
+  }
+}
+
+variable "public_subnets" {
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+    "public_subnet_3" = 3
+  }
+}
+
+# Retrieve the list of AZs in the current AWS region
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
+
+# Define the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name        = var.vpc_name
+    Environment = "demo_environment"
+    Terraform   = "true"
+  }
+}
+
+# Deploy the private subnets
+resource "aws_subnet" "private_subnets" {
+  for_each          = var.private_subnets
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
+  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
+
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
+}
+
+# Deploy the public subnets
+resource "aws_subnet" "public_subnets" {
+  for_each                = var.public_subnets
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
+  availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
+}
+
+# Create route tables
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name      = "demo_public_rtb"
+    Terraform = "true"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name      = "demo_private_rtb"
+    Terraform = "true"
+  }
+}
+
+# Create route table associations
+resource "aws_route_table_association" "public" {
+  for_each       = aws_subnet.public_subnets
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = each.value.id
+}
+
+resource "aws_route_table_association" "private" {
+  for_each       = aws_subnet.private_subnets
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = each.value.id
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "demo_igw"
+  }
+}
+
+# Create EIP for NAT Gateway
+resource "aws_eip" "nat_gateway_eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.internet_gateway]
+  tags = {
+    Name = "demo_igw_eip"
+  }
+}
+
+# Create NAT Gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  depends_on    = [aws_subnet.public_subnets]
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "demo_nat_gateway"
+  }
+}
+
+# Security Group for Load Balancer
+resource "aws_security_group" "ombasa_sg" {
+  description = "Allow HTTP and SSH traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security Group for Instances
+resource "aws_security_group" "instance_sg" {
+  name        = "instance_sg"
+  description = "Allow HTTP traffic to the instance"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port       = var.server_port
+    to_port         = var.server_port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ombasa_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# AMIs
+data "aws_ami" "ubuntu_22_04" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  owners = ["099720109477"]
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+
+# Launch Template
+resource "aws_launch_template" "andylt" {
+  image_id      = data.aws_ami.ubuntu_22_04.id
+  instance_type = aws_instance.web_server.instance_type
+
+  network_interfaces {
+    subnet_id       = aws_subnet.public_subnets["public_subnet_1"].id
+    security_groups = [aws_security_group.ombasa_sg.id, aws_security_group.instance_sg.id]
+  }
+}
+
+# ASG
+resource "aws_autoscaling_group" "web_server_asg" {
+  name             = "web-server-asg"
+  max_size         = 3
+  min_size         = 1
+  desired_capacity = 2
+  launch_template {
+    id      = aws_launch_template.andylt.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier = [aws_subnet.public_subnets["public_subnet_1"].id]
+  target_group_arns   = [aws_lb_target_group.ombasa_tg.arn]
+}
+
+# ALB
+resource "aws_lb" "ombasa_alb" {
+  name               = "ombasa-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ombasa_sg.id]
+  subnets = [
+    aws_subnet.public_subnets["public_subnet_1"].id,
+    aws_subnet.public_subnets["public_subnet_2"].id,
+    aws_subnet.public_subnets["public_subnet_3"].id
+  ]
+  tags = {
+    Name = "ombasa-alb"
+  }
+}
+
+resource "aws_lb_target_group" "ombasa_tg" {
+  name     = "ombasa-tg"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-299"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.ombasa_alb.arn
+  port              = var.server_port
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ombasa_tg.arn
+  }
+}
+
+
+
+# --- BACKEND INFRASTRUCTURE ---
+# Create DynamoDB table for locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+# Create S3 Bucket for State
+resource "aws_s3_bucket" "terraform_state_soigwa" {
+  bucket = "soigwa-terraform-state-bucket"
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state_soigwa.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state_soigwa.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Manual EC2 Server
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "Ubuntu EC2 Server"
+  } 
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu_22_04.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name        = "web-${terraform.workspace}"
+    Environment = terraform.workspace
+  }
+}
