c4416ddac08a9837d49bb5edfc6cdbe4da3d73fd
ANDY OMBASA <159134044+SOIGWA@users.noreply.github.com>
2026-04-24 15:35:17 +0300

    Update Terraform reference in end-to-end test

--- test/webserver_test.go
+++ test/webserver_test.go
@@ -17,7 +17,7 @@ func TestFullStackEndToEnd(t *testing.T) {
 	uniqueID := strings.ToLower(random.UniqueId())
 
 	appOptions := &terraform.Options{
-		TerraformDir:    "github.com/SOIGWA/Ombasa-terraform.io//modules/services/webserver-cluster?ref=v0.0.2",
+		TerraformDir:    "github.com/SOIGWA/Ombasa-terraform.io//modules/services/webserver-cluster?ref=main",
 		TerraformBinary: "terraform",
 		Vars: map[string]interface{}{
 			"cluster_name":       fmt.Sprintf("test-app-%s", uniqueID),
